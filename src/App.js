3fb76dad70544441e19dca7292042cafc2f1471b
english194website <sangeeswty4@gmail.com>
2021-10-16 19:25:26 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>Sangeetha</h1>
+        <p>lorem app isa good.</p>
+        <h2>Updated files</h2>
+        <button>Updated is done.....</button>
         <a
           className="App-link"
           href="https://reactjs.org"
